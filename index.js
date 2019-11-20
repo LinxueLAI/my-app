218e5bac9d582b023b637456f66c2e3e1905d213
Linxue LAI <linxue.lai@depinfonancy.net>
2019-11-20 22:24:14 +0800

    Message

---
+++ index.js
@@ -0,0 +1,22 @@
+    // import library
+    const express = require('express');
+    // Use default PORT Or given from platform
+    const PORT = process.env.PORT || 80;
+    // create application
+    const app = express();
+
+    // some basic route
+    app.get('/',(req, res) => {
+        // send response.
+        res.json({ message: 'coucou' });
+
+    });
+
+    // start server
+    app.listen(80, (err) =>{
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        console.info("server started")
+    });
